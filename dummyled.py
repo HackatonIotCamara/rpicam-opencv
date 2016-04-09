7d516cb6d79ca13d87a5bad3c22fc7d9e3ffb700
Carlos Alberto Lopez Perez <clopez@igalia.com>
2016-04-09 19:42:10 +0200

    Add support for config file, fix mail and add dummy led

---
+++ dummyled.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+class LED():
+	def __init__(self, N):
+		print "LED not available"
+	def on(self):
+		print "LED not available"
+	def off(self):
+		print "LED not available"
+
+if __name__ == '__main__':
+	main()
+
